961c2af1cc116f11bd159ac06e424ef077952eee
VitorBorgesTose <105383171+VitorBorgesTose@users.noreply.github.com>
2023-08-25 11:49:19 -0300

    Tela de cadastro e exibir os resultados

---
+++ Cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form metod="GET" action="exibir.php">
+       nome: <input type="text" name="nome"><br><br>
+       idade: <input type="number" name="idade"><br><br>
+       E-mail: <input type="text" name="email"><br><br>
+       Senha: <input type="password" name="senha"><br><br>
+       <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exibir.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "O nome é: " .$_GET['nome'] . "<br>";
+echo "A idade é: " . $_GET['idade'] . "<br>";
+echo "O email é: " . $_GET['email'] . "<br>";
+echo "Sua senha é: " . $_GET['senha'] . "<br>";
